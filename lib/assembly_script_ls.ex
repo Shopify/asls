fcb22ef350aa1c0b828a2fe3068d7bbab2fc93f8
Saúl Cabrera <saulecabrera@gmail.com>
2020-05-11 08:54:30 -0400

    feat: Diagnostics Parser
    This commit adds an initial diagnostics parser, it handles
    diagnostics and location parsing

--- lib/assembly_script_ls.ex
+++ lib/assembly_script_ls.ex
@@ -1,18 +1,2 @@
 defmodule AssemblyScriptLS do
-  @moduledoc """
-  Documentation for AssemblyScriptLS.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AssemblyScriptLS.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
