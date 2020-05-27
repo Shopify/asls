74f2794bb9c89e10f4a5b6ee1b9834cae52b17b4
Saúl Cabrera <saulecabrera@gmail.com>
2020-05-26 20:45:25 -0400

    feat: Add language server supervision tree

--- lib/assembly_script_ls.ex
+++ lib/assembly_script_ls.ex
@@ -1,2 +1,14 @@
 defmodule AssemblyScriptLS do
+  def start(socket) do
+    socket
+    |> children
+    |> Supervisor.start_link(strategy: :one_for_one)
+  end
+
+  defp children(socket)do
+    [
+      {AssemblyScriptLS.JsonRpc, [socket: socket]}
+    ]
+  end
 end
+
