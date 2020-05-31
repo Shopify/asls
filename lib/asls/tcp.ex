a47097193f1e7d58b39fabdbbd09ff6829811127
Saúl Cabrera <saulecabrera@gmail.com>
2020-05-31 19:33:26 -0400

    feat: Add log level @ TCP

--- lib/asls/tcp.ex
+++ lib/asls/tcp.ex
@@ -27,12 +27,14 @@ defmodule AssemblyScriptLS.TCP do
   could not be established.
   """
   def start(opts \\ []) do
-    port = Keyword.get(opts, :port, @port)
+    port = Keyword.get(opts, :port) || @port
+    debug = Keyword.get(opts, :debug)
     OK.try do
       socket <- :gen_tcp.listen(port, @opts)
       socket <- :gen_tcp.accept(socket)
-      _ <- AssemblyScriptLS.start(socket)
+      _ <- AssemblyScriptLS.start(socket, debug)
     after
+      Logger.debug("Server listening in port #{port}")
       recv(socket)
     rescue
       error ->
@@ -47,18 +49,60 @@ defmodule AssemblyScriptLS.TCP do
   """
   @spec send(socket, packet) :: :ok | {:error, term}
   def send(socket, packet) do
-    :gen_tcp.send(socket, packet)
+    body = Jason.encode!(packet) <> "\r\n\r\n"
+    header = "Content-Length: #{byte_size(body)}\r\n\r\n"
+
+    Logger.debug("Outgoing message: \r\n #{inspect(header)} \r\n #{inspect(body)}")
+
+    :gen_tcp.send(socket, header <> body)
   end
 
   defp recv(socket) do
     OK.try do
-      msg <- :gen_tcp.recv(socket, 0)
+      socket <- set_packet_type(socket, :line)
+      header <- :gen_tcp.recv(socket, 0)
+      length <- content_length(header)
+      socket <- set_packet_type(socket, :raw)
+      # TODO: Fix the hardcoded byte count
+      _ <- :gen_tcp.recv(socket, 2)
+      payload <- :gen_tcp.recv(socket, length)
+      json <- Jason.decode(payload, [keys: :atoms])
     after
-      JsonRpc.recv(msg)
+      Logger.debug("Incoming message: \r\n #{inspect(json)}")
+      JsonRpc.recv(Message.new(json))
       recv(socket)
     rescue
+      # Ignore everything if the first line
+      # is not a valid header
+      :invalid_header ->
+        IO.inspect "Invalid header"
+        recv(socket)
+
+      # Ignore invalid json payloads
+      %Jason.DecodeError{} ->
+        recv(socket)
       error ->
         OK.failure(error)
+    end
+  end
+
+  defp content_length(<<"Content-Length: ", length :: binary>>) do
+    length
+    |> String.trim
+    |> String.to_integer
+    |> OK.success
+  end
+
+  defp content_length(_) do
+    OK.failure(:invalid_header)
+  end
+
+  defp set_packet_type(socket, type) do
+    case :inet.setopts(socket, packet: type) do
+      :ok ->
+        OK.success(socket)
+      {:error, reason} ->
+        OK.failure(reason)
     end
   end
 end
