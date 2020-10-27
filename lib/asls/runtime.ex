7e47a3b2af379352840fdc398f062bb800938ca7
Saúl Cabrera <saulecabrera@gmail.com>
2020-10-26 20:49:20 -0400

    chore: Runtime tests
    Since these tests rely heavily on the file system,
    I'm using mock to simulate the file system rather than
    creating the files themselves.

--- lib/asls/runtime.ex
+++ lib/asls/runtime.ex
@@ -3,71 +3,74 @@ defmodule AssemblyScriptLS.Runtime do
   use OK.Pipe
 
   @asc_paths %{local: "./node_modules/.bin/asc", global: "asc"}
-  @config_file_path "./asconfig.json"
-  @target "asls"
+  @config_file_path "asconfig.json"
+  @type t :: %__MODULE__{
+    root_uri: String.t,
+    executable: String.t,
+    target: String.t
+  }
+   
+  defstruct [:root_uri, :executable, :target]
 
-  # The runtime consists of the original uri
-  # and the executable used.
+  @doc """
+  Ensures the runtime requirements of the language server
+  """
+  @spec ensure(String.t) :: {:ok, __MODULE__.t} | {:error, String.t}
   def ensure(uri) do
-    root(uri)
+    OK.wrap(%__MODULE__{root_uri: uri})
+    ~>> root
     ~>> configuration
     ~>> target
     ~>> executable
   end
 
-  defp root(uri) do
-    decoded = URI.decode(URI.parse(uri).path)
+  defp root(env) do
+    decoded = URI.decode(URI.parse(env.root_uri).path)
     case File.cd(decoded) do
       :ok ->
-        OK.success(decoded)
+        OK.success(env)
       _ ->
         OK.failure("The project root is invalid or doesn't exist.")
     end
   end
 
-  defp executable(_) do
+  defp executable(env) do
     cond do
       File.exists?(@asc_paths.local) ->
-        OK.success(nil)
+        OK.success(%{env | executable: @asc_paths.local})
       true ->
         {_, exit} = System.cmd("which", [@asc_paths.global])
         if exit == 0 do
-          OK.success(nil)
+          OK.success(%{env | executable: @asc_paths.global})
         else
           OK.failure("No executable for AssemblyScript found.")
         end
     end
   end
 
-  defp configuration(_) do
-    contents = File.read(@config_file_path) 
-    if OK.failure?(contents) do
-      OK.failure("No #{@config_file_path} file found.")
+  defp configuration(env) do
+    if File.exists?(@config_file_path) do
+      OK.success(env)
     else
-      contents
+      OK.failure("No #{@config_file_path} file found.")
     end
   end
 
-  defp target(contents) do
-    result = Jason.decode(contents)
+  defp target(env) do
+    contents = File.read!(@config_file_path)
+    result = Jason.decode(contents, keys: :atoms)
     
     if OK.failure?(result) do
       OK.failure("Invalid #{@config_file_path} file.")
     else
       {:ok, payload} = result
-      if payload[:targets][String.to_atom(@target)] do
-        OK.success(nil)
+      targets = Map.get(payload, :targets, %{})
+      target = Map.keys(targets) |> List.first
+      if target do
+        OK.success(%{env | target: Atom.to_string(target)})
       else
         OK.failure(~s(
-          Your asconfig.json file should include a target named asls:
-
-          {
-            ...
-            targets: {
-              asls: {}
-            }
-            ...
-          }
+          Your asconfig.json file should include at least one target definition.
         ))
       end
     end
