53b4bae96a9524b2475d2dbad8b2cea3db156abc
Saúl Cabrera <saulecabrera@gmail.com>
2021-05-10 20:15:12 -0400

    chore: Ensure that underlying directory names match for both platforms

--- bin/release_linux.sh
+++ bin/release_linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker build -f Dockerfile.linux -t asls .
-docker cp $(docker create asls:latest):/app ./bin/asls-linux_x86_64
+docker cp $(docker create asls:latest):/app ./bin/asls
 
 cd bin
-tar vzcf asls-linux_x86_64.tar.gz ./asls-linux_x86_64/
+tar vzcf asls-linux_x86_64.tar.gz ./asls
 
-rm -rf ./asls-linux_x86_64
+rm -rf ./asls
 
 echo "Done"
